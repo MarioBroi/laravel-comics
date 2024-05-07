7a0097323bca4aea6a55bfb00fee0f1937c0f6f6
MarioBroi <inoix16@gmail.com>
2024-05-07 13:59:29 +0100

    edit header section

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,38 @@
 <header class="site-header">
-    
+
+    <div class="container">
+
+        <div class="top">
+            <h5>DC POWER™ VISA®</h5>
+            <h5>ADDITIONAL DC SITES 🔽</h5>
+        </div>
+        <!-- .top -->
+
+        <nav class="navbar navbar-expand-sm navbar-light d-flex">
+            <div class="container">
+                <a class="navbar-brand" href="#">LOGO</a>
+                <ul class="list-unstyled d-flex">
+                    <li class="px-2">CHARATERS</li>
+                    <li class="px-2">COMICS</li>
+                    <li class="px-2">MOVIES</li>
+                    <li class="px-2">TV</li>
+                    <li class="px-2">GAMES</li>
+                    <li class="px-2">COLLECTIBLES</li>
+                    <li class="px-2">VIDEOS</li>
+                    <li class="px-2">FANS</li>
+                    <li class="px-2">NEWS</li>
+                    <li class="px-2">SHOP 🔽</li>
+                </ul>
+                <span>
+                    Search 🔍
+                </span>
+
+            </div>
+        </nav>
+        <!-- .navbar -->
+
+    </div>
+    <!-- /.container -->
+
 </header>
 <!-- /.site-header -->
