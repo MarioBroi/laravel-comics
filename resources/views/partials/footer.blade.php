79587957862bf5c9a0636b3a5ccd8148844cf739
MarioBroi <inoix16@gmail.com>
2024-05-08 11:29:57 +0100

    edit site footer display

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,48 +3,54 @@
 
         <section class="footer-top">
 
-            <ul class="list-unstyled">
-                <li><a href="">Lorem.</a></li>
-                <li><a href="">Voluptatum.</a></li>
-                <li><a href="">Ipsam?</a></li>
-                <li><a href="">Mollitia.</a></li>
-                <li><a href="">Provident?</a></li>
-                <li><a href="">Natus?</a></li>
-                <li><a href="">Animi.</a></li>
-            </ul>
+            <div class="d-flex">
 
-            <ul class="list-unstyled">
-                <li><a href="">Lorem.</a></li>
-                <li><a href="">Cupiditate.</a></li>
-                <li><a href="">Aperiam.</a></li>
-                <li><a href="">Aperiam.</a></li>
-                <li><a href="">Nihil?</a></li>
-                <li><a href="">Dolorem?</a></li>
-                <li><a href="">Quam.</a></li>
-            </ul>
+                <ul class="list-unstyled">
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Voluptatum.</a></li>
+                    <li><a href="">Ipsam?</a></li>
+                    <li><a href="">Mollitia.</a></li>
+                    <li><a href="">Provident?</a></li>
+                    <li><a href="">Natus?</a></li>
+                    <li><a href="">Animi.</a></li>
+                </ul>
 
-            <ul class="list-unstyled">
-                <li><a href="">Lorem.</a></li>
-                <li><a href="">Voluptatum.</a></li>
-                <li><a href="">Ipsam?</a></li>
-                <li><a href="">Mollitia.</a></li>
-                <li><a href="">Provident?</a></li>
-                <li><a href="">Natus?</a></li>
-                <li><a href="">Animi.</a></li>
-            </ul>
+                <ul class="list-unstyled">
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Cupiditate.</a></li>
+                    <li><a href="">Aperiam.</a></li>
+                    <li><a href="">Aperiam.</a></li>
+                    <li><a href="">Nihil?</a></li>
+                    <li><a href="">Dolorem?</a></li>
+                    <li><a href="">Quam.</a></li>
+                </ul>
 
-            <ul class="list-unstyled">
-                <li><a href="">Lorem.</a></li>
-                <li><a href="">Cupiditate.</a></li>
-                <li><a href="">Aperiam.</a></li>
-                <li><a href="">Aperiam.</a></li>
-                <li><a href="">Nihil?</a></li>
-                <li><a href="">Dolorem?</a></li>
-                <li><a href="">Quam.</a></li>
-            </ul>
+                <ul class="list-unstyled">
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Voluptatum.</a></li>
+                    <li><a href="">Ipsam?</a></li>
+                    <li><a href="">Mollitia.</a></li>
+                    <li><a href="">Provident?</a></li>
+                    <li><a href="">Natus?</a></li>
+                    <li><a href="">Animi.</a></li>
+                </ul>
+
+                <ul class="list-unstyled">
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Cupiditate.</a></li>
+                    <li><a href="">Aperiam.</a></li>
+                    <li><a href="">Aperiam.</a></li>
+                    <li><a href="">Nihil?</a></li>
+                    <li><a href="">Dolorem?</a></li>
+                    <li><a href="">Quam.</a></li>
+                </ul>
+
+            </div>
 
             <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint architecto eum minus, quo cupiditate similique vero ipsam! Est quas temporibus nisi corrupti minima libero facilis asperiores rem natus? Doloribus, culpa.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint architecto eum minus, quo cupiditate
+                similique vero ipsam! Est quas temporibus nisi corrupti minima libero facilis asperiores rem natus?
+                Doloribus, culpa.
             </p>
 
         </section>
@@ -56,12 +62,12 @@
                     SIGN-UP NOW
                 </button>
                 <div>
-                        <a href="">FOLLOW US</a>
-                        <a href="">logo</a>
-                        <a href="">logo</a>
-                        <a href="">logo</a>
-                        <a href="">logo</a>
-                        <a href="">logo</a>
+                    <a href="">FOLLOW US</a>
+                    <a href="">logo</a>
+                    <a href="">logo</a>
+                    <a href="">logo</a>
+                    <a href="">logo</a>
+                    <a href="">logo</a>
                 </div>
             </div>
         </section>
