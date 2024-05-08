9bdce087034a84b3fba2c467f65ccc55e3947b68
MarioBroi <inoix16@gmail.com>
2024-05-08 14:10:52 +0100

    edit comic.blade.php style, add font awesome cdn link

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -3,21 +3,33 @@
 @section('content')
     <jumbotron class="jumbotron">
         <div class="jumbo-container">
-
         </div>
     </jumbotron>
     <!-- /.jumbotron -->
 
     <div class="img-container">
-        <img src="{{ $comic['thumb'] }}" alt="" class="thumb-jumbo">
+        <div class="thumb-card">
+            <img src="{{ $comic['thumb'] }}" alt="" class="">
+            <a href="">
+                <div>
+                    VIEW GALLERY
+                </div>
+            </a>
+        </div>
+        <div class="book-type">
+            {{ $comic['type'] }}
+        </div>
     </div>
     <!-- /.img-container -->
 
-    <section class="description">
-        <div class="container">
-            <h2>{{ $comic['title'] }}</h2>
+    <div class="description d-flex">
+
+        <div class="description-left">
+
+            <h2 class="description-title">{{ $comic['title'] }}</h2>
+
             <div class="availability">
-                <div class="available justify-content-between px-4">
+                <div class="available">
                     <div class="comic-price">
                         U.S. Price: {{ $comic['price'] }}
                     </div>
@@ -25,14 +37,19 @@
                         AVAILABLE
                     </div>
                 </div>
-                <div>
-                    Check Availability 🔽
+                <div class="check-availability">
+                    <a href="">Check Availability <i class="fa-solid fa-chevron-down"></i></a>
                 </div>
             </div>
-            <p>
+            <p class="comic-description">
                 {{ $comic['description'] }}
             </p>
         </div>
-    </section>
+
+        <div class="adv d-flex flex-column align-items-end">
+            <div>ADVERTISEMENT</div>
+            <img src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="">
+        </div>
+    </div>
     <!-- /.description -->
 @endsection
