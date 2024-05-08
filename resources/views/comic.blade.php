1f0807f4c4afd8765f14a9cbc9427446c991fa17
MarioBroi <inoix16@gmail.com>
2024-05-08 12:58:05 +0100

    edic comic section with style

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -1,9 +1,38 @@
 @extends('layout.app')
 
 @section('content')
-    {{-- TESTING THE CORRECT ROUTE --}}
-    <div class="container">
-        <img src="{{ $comic['thumb'] }}" alt="">
+    <jumbotron class="jumbotron">
+        <div class="jumbo-container">
+
+        </div>
+    </jumbotron>
+    <!-- /.jumbotron -->
+
+    <div class="img-container">
+        <img src="{{ $comic['thumb'] }}" alt="" class="thumb-jumbo">
     </div>
-    <!-- /.container -->
+    <!-- /.img-container -->
+
+    <section class="description">
+        <div class="container">
+            <h2>{{ $comic['title'] }}</h2>
+            <div class="availability">
+                <div class="available justify-content-between px-4">
+                    <div class="comic-price">
+                        U.S. Price: {{ $comic['price'] }}
+                    </div>
+                    <div>
+                        AVAILABLE
+                    </div>
+                </div>
+                <div>
+                    Check Availability 🔽
+                </div>
+            </div>
+            <p>
+                {{ $comic['description'] }}
+            </p>
+        </div>
+    </section>
+    <!-- /.description -->
 @endsection
