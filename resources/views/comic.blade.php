dfacc68dee483aab6cdb0c80b77765a6dc38851e
MarioBroi <inoix16@gmail.com>
2024-05-08 15:05:58 +0100

    add info and edit layout in comic.blade.php

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -48,8 +48,72 @@
 
         <div class="adv d-flex flex-column align-items-end">
             <div>ADVERTISEMENT</div>
-            <img src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="">
+            <a href="">
+                <img src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="">
+            </a>
         </div>
     </div>
     <!-- /.description -->
+
+    <div class="info">
+        <div class="info-container">
+
+            <div class="info-left">
+                <ul>
+                    <li class="info-top">
+                        <h4>Talent</h4>
+                    </li>
+                    <li>
+                        <h6>Art by:</h6>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit alias non deserunt repellat
+                            commodi consectetur.</p>
+
+                    </li>
+                    <li>
+                        <h6>Written by:</h6>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id ea voluptates quo voluptatum error
+                            excepturi?</p>
+
+                    </li>
+                </ul>
+
+            </div>
+            <!-- /.info-left -->
+
+            <div class="info-right">
+                <ul>
+                    <li class="info-top">
+                        <h4>Specs</h4>
+                    </li>
+                    <li>
+                        <h6>
+                            Series:
+                        </h6>
+                        <p>
+                            {{ $comic['series'] }}
+                        </p>
+                    </li>
+                    <li>
+                        <h6>
+                            U.S. Price:
+                        </h6>
+                        <p>
+                            {{ $comic['price'] }}
+                        </p>
+                    </li>
+                    <li>
+                        <h6>
+                            On Sale Date:
+                        </h6>
+                        <p>
+                            {{ $comic['sale_date'] }}
+                        </p>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.info-right -->
+        </div>
+        <!-- /.info-container -->
+    </div>
+    <!-- /.info -->
 @endsection
